1293efb16d3ea967f890c26432f9826adc976855
Hümeyra Ayyıldız <humeyrayildiz1996@gmail.com>
2026-07-01 12:34:45 +0300

    düzeltmeeee

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import FriendList from "./components/FriendList/FriendList.jsx";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
 import UserData from "./userData.json";
 import friends from "./friends.json";
-import transactions from "./transaction.json";
+import transactions from "./components/transaction.json";
 
 export const App = () => {
   return (
